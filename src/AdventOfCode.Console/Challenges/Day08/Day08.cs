5aeaacdeafb0f9e475e70670f3ce9c1273857c4a
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-12-07 10:14:15 +0100

    Prep day 08

---
+++ src/AdventOfCode.Console/Challenges/Day08/Day08.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using AdventOfCode.Core;
+
+namespace AdventOfCode.Challenges;
+
+[Description("Day 08")]
+public class Day08 : Challenge<Day08>
+{
+    public Day08(string[] input) : base(input)
+    {
+    }
+
+    public Day08() : base()
+    {
+    }
+    
+    public override int SolvePart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
