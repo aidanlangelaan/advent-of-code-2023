54c228954b0e7881543b052da1fe2a8a5cd2d9eb
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-12-12 10:17:25 +0100

    Day 12 - Part 1 (memory issues for part 2)

--- src/AdventOfCode.Console/Challenges/Day12/Day12.cs
+++ src/AdventOfCode.Console/Challenges/Day12/Day12.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Text;
 using AdventOfCode.Core;
 
 namespace AdventOfCode.Challenges;
@@ -16,11 +17,103 @@ public class Day12 : Challenge<Day12>
     
     public override int SolvePart1()
     {
-        throw new NotImplementedException();
+        var arrangementCount = new List<int>();
+        foreach (var line in _input)
+        {
+            var parts = line.Split(" ");
+            var springs = parts[0];
+            var damagedGroups = parts[1].Split(',').Select(int.Parse).ToList();
+
+            var options = GetAllOptions(springs);
+
+            var viableOptionCount = GetValidOptionCount(options, damagedGroups);
+            arrangementCount.Add(viableOptionCount);
+        }
+        
+        return arrangementCount.Sum();
     }
 
     public override int SolvePart2()
     {
-        throw new NotImplementedException();
+        var arrangementCount = new List<int>();
+        foreach (var line in _input)
+        {
+            var parts = line.Split(" ");
+            var springs = Unfold(parts[0], '?');
+            var damagedGroups = Unfold(parts[1], ',').Split(',').Select(int.Parse).ToList();
+
+            var options = GetAllOptions(springs);
+
+            var viableOptionCount = GetValidOptionCount(options, damagedGroups);
+            arrangementCount.Add(viableOptionCount);
+        }
+        
+        return arrangementCount.Sum();
+    }
+
+    private string Unfold(string input, char separator)
+    {
+        var unfoldedInput = string.Empty;
+        for (var i = 0; i < 5; i++)
+        {
+            unfoldedInput += input;
+            if (i < 4)
+            {
+                unfoldedInput += separator;
+            }
+        }
+
+        return unfoldedInput;
+    }
+    
+    private int GetValidOptionCount(List<string> options, List<int> damagedGroups)
+    {
+        var viableOptionCount = 0;
+        foreach (var option in options)
+        {
+            var groups = option
+                .Split('.')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToList()
+                .Select(x => x.Length);
+
+            if (groups.SequenceEqual(damagedGroups))
+            {
+                viableOptionCount += 1;
+            }
+        }
+
+        return viableOptionCount;
+    }
+
+    private List<string> GetAllOptions(string springs)
+    {
+        var options = new List<string>();
+        var unknowns = springs.Count(c => c == '?');
+        var possibleOptions = Math.Pow(2, unknowns);
+        for (var i = 0; i < possibleOptions; i++)
+        {
+            options.Add(GetOption(springs, i));
+        }
+
+        return options;
+    }
+    
+    private string GetOption(string springs, int option)
+    {
+        var result = string.Empty;
+        foreach (var spring in springs)
+        {
+            if (spring == '?')
+            {
+                result += option % 2 == 1 ? '#' : '.';
+                option >>= 1;
+            }
+            else
+            {
+                result += spring;
+            }
+        }
+        return result;
     }
 }
--- src/AdventOfCode.Console/Program.cs
+++ src/AdventOfCode.Console/Program.cs
@@ -43,12 +43,12 @@ Console.WriteLine("--- Advent of Code 2023 ---");
 // day10.SolveDayPart1();
 // day10.SolveDayPart2();
 
-var day11 = new Solver<Day11>(true);
-day11.SolveDayPart1();
-day11.SolveDayPart2();
+// var day11 = new Solver<Day11>(true);
+// day11.SolveDayPart1();
+// day11.SolveDayPart2();
 
-// var day12 = new Solver<Day12>(true);
-// day12.SolveDayPart1();
+var day12 = new Solver<Day12>(true);
+day12.SolveDayPart1();
 // day12.SolveDayPart2();
 
 // var day13 = new Solver<Day13>(true);
