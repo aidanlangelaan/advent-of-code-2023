610586f3b0c13f46441ab88372036c9d0854314f
Aidan Langelaan <aidanlangelaan@gmail.com>
2024-11-28 17:17:21 +0100

    The uncompleted day 13 attempt

--- src/AdventOfCode.Console/Program.cs
+++ src/AdventOfCode.Console/Program.cs
@@ -47,12 +47,12 @@ Console.WriteLine("--- Advent of Code 2023 ---");
 // day11.SolveDayPart1();
 // day11.SolveDayPart2();
 
-var day12 = new Solver<Day12>(true);
-//day12.SolveDayPart1();
-day12.SolveDayPart2();
+// var day12 = new Solver<Day12>(true);
+// day12.SolveDayPart1();
+// day12.SolveDayPart2();
 
-// var day13 = new Solver<Day13>(true);
-// day13.SolveDayPart1();
+var day13 = new Solver<Day13>(true);
+day13.SolveDayPart1();
 // day13.SolveDayPart2();
 
 // var day14 = new Solver<Day14>(true);
